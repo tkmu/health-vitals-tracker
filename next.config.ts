2befd3691aea3fe38e4f317b882ff3771810c0ff
Tanmay Khandelwal <tanmaykhandelwal16@gmail.com>
2026-05-17 10:33:51 +0530

    Add tesseract.js dependencies to Next.js trace

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,14 @@ const nextConfig: NextConfig = {
     "/api/**/*": [
       "./node_modules/pdfjs-dist/legacy/build/**/*",
       "./node_modules/tesseract.js/**/*",
-      "./node_modules/tesseract.js-core/**/*"
+      "./node_modules/tesseract.js-core/**/*",
+      "./node_modules/bmp-js/**/*",
+      "./node_modules/is-url/**/*",
+      "./node_modules/node-fetch/**/*",
+      "./node_modules/regenerator-runtime/**/*",
+      "./node_modules/wasm-feature-detect/**/*",
+      "./node_modules/zlibjs/**/*",
+      "./node_modules/idb-keyval/**/*"
     ],
   },
 };
